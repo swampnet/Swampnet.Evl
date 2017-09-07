3a7ef314c0aa89c73c61b31c6a8d25983d672d94
Pj <pwhitby@optimal-ltd.co.uk>
2017-09-07 15:52:49 +0100

    Refactoring some static data Add meta controller to main api

--- Swampnet.Evl.Common/Entities/Meta.cs
+++ Swampnet.Evl.Common/Entities/Meta.cs
@@ -11,10 +11,144 @@ namespace Swampnet.Evl.Common.Entities
 		public ActionMetaData[] ActionMetaData { get; set; }
 		public ExpressionOperator[] Operators { get; set; }
 		public MetaDataCapture[] Operands { get; set; }
-	}
+
+        public static MetaData Default => _meta;
+
+        // Hard coding all this, but should really generate it all dynamically.
+        private static readonly MetaData _meta = new MetaData()
+        {
+            ActionMetaData = new[]
+            {
+                new ActionMetaData()
+                {
+                    Type = "email",
+                    Properties = new[]
+                    {
+                        new MetaDataCapture()
+                        {
+                            Name = "to",
+                            IsRequired = true,
+                            Options = new[]
+                            {
+                                new Option(null, ".*@.*")
+                            }
+                        },
+                        new MetaDataCapture()
+                        {
+                            Name = "cc",
+                            IsRequired = false,
+                            Options = new[]
+                            {
+                                new Option(null, ".*@.*")
+                            }
+                        },
+                        new MetaDataCapture()
+                        {
+                            Name = "bcc",
+                            IsRequired = false,
+                            Options = new[]
+                            {
+                                new Option(null, ".*@.*")
+                            }
+                        },
+                    }
+                },
+
+                new ActionMetaData()
+                {
+                    Type = "change-category",
+                    Properties = new[]
+                    {
+                        new MetaDataCapture()
+                        {
+                            Name = "Category",
+                            IsRequired = true,
+                            DataType = "select",
+                            Options = new[]
+                            {
+                                new Option("Information", "Information"),
+                                new Option("Error", "Error"),
+                                new Option("Debug", "Debug")
+                            }
+                        }
+                    }
+                },
+
+                new ActionMetaData()
+                {
+                    Type = "add-property",
+                    Properties = new[]
+                    {
+                        new MetaDataCapture()
+                        {
+                            Name = "category",
+                            IsRequired = false,
+                        },
+                        new MetaDataCapture()
+                        {
+                            Name = "name",
+                            IsRequired = true,
+                        },
+                        new MetaDataCapture()
+                        {
+                            Name = "value",
+                            IsRequired = true,
+                        }
+                    }
+                }
+            },
+
+            Operators = new[]
+            {
+                new ExpressionOperator(RuleOperatorType.MATCH_ALL, "Match All", true),
+                new ExpressionOperator(RuleOperatorType.MATCH_ANY, "Match Any", true),
+
+                new ExpressionOperator(RuleOperatorType.EQ, "=", false),
+                new ExpressionOperator(RuleOperatorType.NOT_EQ, "<>", false),
+                new ExpressionOperator(RuleOperatorType.GT, ">", false),
+                new ExpressionOperator(RuleOperatorType.GTE, ">=", false),
+                new ExpressionOperator(RuleOperatorType.LT, "<", false),
+                new ExpressionOperator(RuleOperatorType.LTE, "<=", false),
+                new ExpressionOperator(RuleOperatorType.REGEX, "Match Expression", false)
+            },
+
+            Operands = new[]
+            {
+                new MetaDataCapture()
+                {
+                    Name = "Summary"
+                },
+
+                new MetaDataCapture()
+                {
+                    Name = "Timestamp",
+                    DataType = "datetime"
+                },
+
+                new MetaDataCapture()
+                {
+                    Name = "Category",
+                    DataType = "select",
+                    Options = new[]
+                    {
+                        new Option("Information", "Information"),
+                        new Option("Error", "Error"),
+                        new Option("Debug", "Debug")
+                    }
+                },
+
+                new MetaDataCapture()
+                {
+                    Name = "Property",
+                    DataType = "require-args"
+                }
+            }
+        };
+
+    }
 
 
-	public class Option
+    public class Option
 	{
 		public Option()
 		{
---
+++ Swampnet.Evl/Controllers/MetaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swampnet.Evl.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Swampnet.Evl.Controllers
+{
+    [Route("api/meta")]
+    public class MetaController : Controller
+    {
+        // GET api/meta
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(MetaData.Default);
+        }
+    }
+}
