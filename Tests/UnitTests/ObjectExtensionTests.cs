c8481c124d3c50815d5bbd4118abec28b274d8f6
aime-johnson <aime.johnson@theswamp.co.uk>
2017-10-25 11:36:32 +0100

    ObjectExtension tests

---
+++ Tests/UnitTests/ObjectExtensionTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Swampnet.Evl;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ObjectExtensionTests
+    {
+        [TestMethod]
+        public void ObjectExtension_ToPublicProperties()
+        {
+            var o = new SimpleClass()
+            {
+                SomeInt = 123,
+                SomeString = "string"
+            };
+
+            var prps = o.GetPublicProperties();
+
+            Assert.IsNotNull(prps);
+            Assert.AreEqual(2, prps.Count());
+
+            var someIntProperty = prps.SingleOrDefault(p => p.Name == "SomeInt");
+            Assert.IsNotNull(someIntProperty);
+            Assert.AreEqual("SimpleClass", someIntProperty.Category);
+            Assert.AreEqual("123", someIntProperty.Value);
+
+            var someStringProperty = prps.SingleOrDefault(p => p.Name == "SomeString");
+            Assert.AreEqual("SimpleClass", someStringProperty.Category);
+            Assert.IsNotNull(someStringProperty);
+            Assert.AreEqual("string", someStringProperty.Value);
+        }
+    }
+
+    public class SimpleClass
+    {
+        public int SomeInt { get; set; }
+        public string SomeString { get; set; }
+        private int SomePrivateProperty { get; set; }
+    }
+}
