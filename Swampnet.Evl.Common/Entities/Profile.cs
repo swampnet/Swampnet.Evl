42644c9d9e0dbfdd013db0042f7a0a3cfa728e56
aime-johnson <aime.johnson@theswamp.co.uk>
2017-10-27 11:52:51 +0100

    Add Profile and Group entities

---
+++ Swampnet.Evl.Common/Entities/Profile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swampnet.Evl.Common.Entities
+{
+    public class Profile
+    {
+        public long Id { get; set; }
+
+        public List<Group> Groups { get; set; }
+
+        public Name Name { get; set; }
+
+        public Organisation Organisation { get; set; }
+    }
+
+
+    public class Name
+    {
+        public string Title { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string KnownAs { get; set; }
+    }
+}
