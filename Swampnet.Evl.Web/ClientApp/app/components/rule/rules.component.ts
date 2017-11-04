0da0188331c6885408d4ba4cdab7e3b06389db76
Pj <petejw@theswamp.co.uk>
2017-11-04 16:51:00 +0000

    ..

--- Swampnet.Evl.Web/ClientApp/app/components/rule/rules.component.ts
+++ Swampnet.Evl.Web/ClientApp/app/components/rule/rules.component.ts
@@ -11,49 +11,56 @@ import { DragulaService } from 'ng2-dragula/ng2-dragula';
 })
 export class RulesComponent implements OnInit, OnDestroy {
     public rules: RuleSummary[];
+	private dropSubscription: any;
 
     constructor(
         private router: Router,        
 		private api: ApiService,
-		private dragula: DragulaService
-	) {
+		private dragula: DragulaService) {
+		console.log("RulesComponent.ctor");
+
 		this.dragula.setOptions('bag-items', {
 			revertOnSpill: true
 		});
+    }
 
-		this.dragula
+
+	ngOnInit() {
+		console.log("RulesComponent.OnInit");
+
+		this.dropSubscription = this.dragula
 			.dropModel
 			.subscribe((x: any) => {
 				this.reorder();
 			});
-    }
 
-
-	reorder() {
-		var i: number;
-		for (i = 0; i < this.rules.length; i++) {
-			this.rules[i].order = i;
-		}
-		this.api.reorderRules(this.rules);
-	}
-
-
-	ngOnInit() {
-        this.api.getRules().then((res: RuleSummary[]) => {
+		this.api.getRules().then((res: RuleSummary[]) => {
 			this.rules = res;
         }, (error) => {
             console.log("Failed to get rule", error._body, "error");
         });
-
 	}
 
 
 	ngOnDestroy() {
+		console.log("RulesComponent.OnDestroy");
+
+		this.dropSubscription.unsubscribe();
 		this.dragula.destroy('bag-items');
 	}
 
 
     newRule(){
         this.router.navigate(['/rules/new']);
-    }
+	}
+
+
+	reorder() {
+		console.log("reorder");
+		var i: number;
+		for (i = 0; i < this.rules.length; i++) {
+			this.rules[i].order = i;
+		}
+		this.api.reorderRules(this.rules);
+	}
 }
