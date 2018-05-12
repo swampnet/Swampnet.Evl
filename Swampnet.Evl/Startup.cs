56ef1b435be98128b82d7780443c2bfc41ae130d
Pj <petejw@theswamp.co.uk>
2018-05-12 21:57:53 +0100

    ...

--- Swampnet.Evl/Startup.cs
+++ Swampnet.Evl/Startup.cs
@@ -121,16 +121,16 @@ namespace Swampnet.Evl
 			if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                // Enable middleware to serve generated Swagger as a JSON endpoint.
+                app.UseSwagger();
+
+                // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Evl API V1");
+                });
             }
-
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Evl API V1");
-            });
 
 
             app.UseCors(cfg =>
