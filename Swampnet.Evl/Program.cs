c7e00345437e2fb7d308014b96a06222a2df57ce
Pete Whitby <petejw@theswamp.co.uk>
2018-10-13 15:19:21 +0100

    Add insights

--- Swampnet.Evl/Program.cs
+++ Swampnet.Evl/Program.cs
@@ -19,6 +19,7 @@ namespace Swampnet.Evl
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>()
                 .Build();
     }
