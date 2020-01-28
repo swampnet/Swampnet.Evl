a463fa0134d624a6a5f5c74d7234620436ccff58
Pete Whitby <pete.whitby@octavian.co.uk>
2020-01-28 14:59:07 +0000

    Add FunctionsStartup / DI

---
+++ Swampnet.Evl.Functions/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(Swampnet.Evl.Functions.Startup))]
+namespace Swampnet.Evl.Functions
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+        }
+    }
+}
